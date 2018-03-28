b834d05dbea528e0e7d44006881c2e41e9f63264
Estevan Bellino <bellino.estevan@gmail.com>
2018-03-28 00:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // write your code below!
 function happyHolidays(string){
-  return
+  return"happyHolidays!";
+}
+function happyHolidayTo(string){
+  return 'Happy Holidays, ${name}!'
+}
+function happyHolidayTo(string){
+  return 'Happy ${holiday}, ${name}!'
+}
+function happyHolidayCountdown(string){
+  return "It's ${days} days until ${holiday}!"
 }
